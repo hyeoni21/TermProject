8864fa6f591040ec0ebd0233257a931cf71204b2
LeeDongHyun <jojo7522@gmail.com>
2018-06-20 08:12:38 +0900

    base

--- code/client.c
+++ code/client.c
@@ -7,12 +7,35 @@
 
 int main(int argc, char **argv)
 {
+	int sock;
+
+	struct sockaddr_in serv_addr;
+
+	
 	if(argc != 3)
 	{
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
+	sock=socket(PF_INET, SOCK_STREAM, 0);
 
+	
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+
+	serv_addr.sin_family=AF_INET;
+
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+
+	serv_addr.sin_port=htons(atoi(argv[2]));
+
+	  
+
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+
+		error_handling("connect() error");
+
+	
 	printf("client\n");
 
 	return 0;
--- code/server.c
+++ code/server.c
@@ -7,12 +7,46 @@
 
 int main(int argc, char **argv)
 {
+	int serv_sock, clnt_sock;
+
+	struct sockaddr_in serv_adr, clnt_adr;
+
+	int clnt_adr_sz;
+
 	if(argc!=2)
 	{
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
 
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+
+	serv_adr.sin_family=AF_INET; 
+
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+
+	serv_adr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+
+		error_handling("bind() error");
+
+	if(listen(serv_sock, 5)==-1)
+
+		error_handling("listen() error");
+
+	while(1)
+	{
+		clnt_adr_sz=sizeof(clnt_adr);
+
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+
+		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+	}
 	printf("server\n");
 
 	return 0;
